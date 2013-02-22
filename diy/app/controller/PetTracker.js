9c7884a0cd98d93dd6a88bea409a817e67a496b7
unknown <8mc8@queensu.ca>
2013-02-22 17:29:10 -0500

    'popup'

--- diy/app/controller/PetTracker.js
+++ diy/app/controller/PetTracker.js
@@ -125,7 +125,7 @@ Ext.define('MyApp.controller.PetTracker', {
         this.markers.push(dynaMarker);
 
         var infowindow = new google.maps.InfoWindow({
-            content: record.data.description
+            content: record.data.description +""
         });
 
         google.maps.event.addListener(dynaMarker, 'click', function () {
