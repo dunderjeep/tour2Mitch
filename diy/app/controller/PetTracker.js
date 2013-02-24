5ee86ddc0d7aa51cd40e43a65604ed12fa58f2c0
unknown <8mc8@queensu.ca>
2013-02-24 14:34:45 -0500

    tag

--- diy/app/controller/PetTracker.js
+++ diy/app/controller/PetTracker.js
@@ -128,7 +128,7 @@ Ext.define('MyApp.controller.PetTracker', {
         this.markers.push(dynaMarker);
 
         var infowindow = new google.maps.InfoWindow({
-            content: "We've found your dog sniffing flowers!"
+            content: record.description +"me"
         });
 
         google.maps.event.addListener(dynaMarker, 'click', function () {
